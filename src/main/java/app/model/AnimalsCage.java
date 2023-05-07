29434489af2525c83b9a01eb883323101bb82dd7
Gala Arbatskaya <galinaarbatskaya0@gmail.com>
2023-05-07 09:05:55 +0300

    timer changes1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,3 +1,4 @@
+
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,26 +7,25 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-
-    @Autowired
-    @Qualifier("dog")
-    private Animal animal;
-
-    @Autowired
+    private final Animal animal;
     private final Timer timer;
-    public AnimalsCage(Timer timer) {
+
+    @Autowired
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
         this.timer = timer;
     }
+
     public Timer getTimer() {
         return timer;
     }
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
-
 }
